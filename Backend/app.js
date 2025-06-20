0e244c438276e8802ee185233454377330f0330d
Shahil Ahamad <sahilahmad31413@gmail.com>
2025-06-20 17:36:32 +0530

    originLive

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 const corsOptions={
-    origin: ['http://localhost:5173','https://uber02.netlify.app'],
+    origin: ['http://localhost:5173','https://uber-2.netlify.app'],
     credentials:true
 }
 app.use(cors(corsOptions))
