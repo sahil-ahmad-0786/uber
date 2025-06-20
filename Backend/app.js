7ed0514808d6267aeed85ff15283b2af00b96acb
Shahil Ahamad <sahilahmad31413@gmail.com>
2025-06-20 16:23:18 +0530

    origin add

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,13 @@ connectToDb();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(cookieParser());
+const corsOptions={
+    origin: ['http://localhost:5173'],
+    credentials:true
+}
+app.use(cors(corsOptions))
 
 
 
